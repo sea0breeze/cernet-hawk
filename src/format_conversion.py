3ea78e5c262e4067dcc3ae9eed54d1586f7fb0e9
chaosjtu <648477609@qq.com>
2016-05-16 22:27:15 +0800

    Add files via upload

--- src/format_conversion.py
+++ src/format_conversion.py
@@ -1,23 +1,25 @@
 import config
+def Conversion(portfile, ipfile):
+    ipdict = {}
+    inputfile = open(portfile)
 
-ipdict = {}
-inputfile = open(config.PORTFILE_PATH)
+    for line in inputfile:
+        line = line.split()
+        port = line[0]
+        iplist = line[1:]
+        for ip in iplist:
+            if ip in ipdict:
+                ipdict[ip] += [port]
+            else:
+                ipdict[ip] = [port]
 
-for line in inputfile:
-    line = line.split()
-    port = line[0]
-    iplist = line[1:]
-    for ip in iplist:
-        if ip in ipdict:
-            ipdict[ip] += [port]
-        else:
-            ipdict[ip] = [port]
+    inputfile.close()
 
-inputfile.close()
+    outputfile = open(ipfile, 'w')
+    for ip in ipdict:
+        line = ' '.join([ip] + ipdict[ip]) + '\n'
+        outputfile.write(line)
+    outputfile.close()
 
-outputfile = open(config.IPFILE_PATH, 'w')
-for ip in ipdict:
-    line = ' '.join([ip] + ipdict[ip]) + '\n'
-    outputfile.write(line)
-
-outputfile.close()
+if __name__ == "__main__":
+    Conversion("port.txt", "ip.txt")
