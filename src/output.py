995d2e905bcfb787a1950ab3ea05dec2d8bdd7e1
chaosjtu <648477609@qq.com>
2016-05-25 20:37:37 +0800

    Add files via upload

--- src/output.py
+++ src/output.py
@@ -2,10 +2,16 @@ import json
 import pprint
   
 def output(outputdict,outputformat,outputfile):
+    '''
+    Output a dictionary in the form of json or pprint
+    :param outputdict: dict. the output dictionary
+    :param outputformat: str. the form of output, must be one of ['j','json','p','pprint']
+    :param outputfile: str. the name of outputfile
+    '''
     outfile = open(outputfile, 'w')
-    if(outputformat=='json'):
+    if(outputformat in ('j', 'json')):
         outfile.write(json.dumps(outputdict))
-    if(outputformat=='pprint'):
+    if(outputformat in ('p', 'pprint')):
        outfile.write(pprint.pformat(outputdict))
     outfile.close()
     
