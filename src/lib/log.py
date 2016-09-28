53e7bf687dd81e956c8ad971ec1cf701a064fc42
LyleMi <1278919492@qq.com>
2016-09-28 17:49:38 +0800

    move

--- src/lib/log.py
+++ src/lib/log.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import pickle
 import logging
 
 from enums import *
@@ -42,29 +41,3 @@ def cout(msg, level=PRINT_LEVEL.DEBUG, color):
     print msg
 
 
-def save(val, identifier):
-    """
-    save variable into a file
-    :param val: the val you want to save
-    :param identifier: str. the identifier of variable
-    :return: bool. status of save
-    """
-    try:
-        output = open('data/pickle/' + identifier + '.pkl', 'wb')
-        pickle.dump(val, output)
-        output.close()
-        return True
-    except:
-        return False
-
-
-def get(identifier):
-    """
-    get variable from a file
-    :param identifier: str. the identifier of variable
-    :return: variable
-    """
-    pkl_file = open('data/pickle/' + name + '.pkl', 'rb')
-    data = pickle.load(pkl_file)
-    pkl_file.close()
-    return data
