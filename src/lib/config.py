e50f6e225ce752b98f8d2cecbd6f0c9267b09eb4
LyleMi <1278919492@qq.com>
2016-09-29 12:17:20 +0800

    upate

--- src/lib/config.py
+++ src/lib/config.py
@@ -1,6 +1,13 @@
+import logging
+
 
 NMAP_CMD = ['-sS', '-Pn', '-sV', '-oX', '-']
 ZMAP_CMD = ['-q', '-v', '0'] # silent
 PORTS = [80,
          22,
          443]
+
+# only when cpint's level >= this, will print
+CONSOLE_PRINT = logging.INFO
+
+LOG_FILE = 'data/log/test.log'
--- src/lib/log.py
+++ src/lib/log.py
@@ -2,19 +2,33 @@
 
 import logging
 
-from enums import PRINT_LEVEL
+from config import CONSOLE_PRINT
+from config import LOG_FILE
+
+class PRINT_LEVEL:
+    DEBUG = 10
+    INFO = 20
+    WARNING = 30
+    ERROR = 40
+    CRITICAL = 50
 
 
 logger = logging.getLogger("hawk_logger")
 
 logger.setLevel(logging.DEBUG)
 
-fh = logging.FileHandler('data/log/test.log')
+fh = logging.FileHandler(LOG_FILE)
+fh.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(CONSOLE_PRINT)
 
 formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
 fh.setFormatter(formatter)
+ch.setFormatter(formatter)
 
 logger.addHandler(fh)
+logger.addHandler(ch)
 
 
 def cprint(msg, level=PRINT_LEVEL.DEBUG):
@@ -29,15 +43,15 @@ def cprint(msg, level=PRINT_LEVEL.DEBUG):
     elif level == PRINT_LEVEL.INFO:
         logger.info(msg)
     elif level == PRINT_LEVEL.WARNING:
-        logger.waring(msg)
+        logger.warning(msg)
     elif level == PRINT_LEVEL.ERROR:
         logger.error(msg)
     elif level == PRINT_LEVEL.CRITICAL:
         logger.critical(msg)
 
-    if level <= PRINT_LEVEL.WARNING:
-        return
+    #if level <= PRINT_LEVEL.WARNING:
+    #    return
 
-    print msg
+    #print msg
 
 
