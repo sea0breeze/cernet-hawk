c51f34f91259ed5923aa4c9d394f53f11193b851
LyleMi <1278919492@qq.com>
2016-10-11 22:36:05 +0800

    add redis

--- src/lib/modules/redis.py
+++ src/lib/modules/redis.py
@@ -1,6 +1,9 @@
 import redis
 
-def redisDetect(host, port=6379)
+def redisDetect(host, port=6379):
+    # this redis code is not complete
+    # redis could be hacked if not config well
+    # http://www.oschina.net/news/67975/redis-defect?from=mail-notify
     try:
         r = redis.StrictRedis(host=host, port=6379, db=0)
         return r.config_get()
