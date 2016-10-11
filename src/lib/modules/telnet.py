642ba1e7936a2b2cbd61a8a142cc8a23a851ec9f
LyleMi <1278919492@qq.com>
2016-10-11 22:47:04 +0800

    add telnet

---
+++ src/lib/modules/telnet.py
@@ -0,0 +1,14 @@
+import telnetlib
+
+
+def telnetDetect(host, port=2611,  timeout=40,):
+    try:
+        tn = telnetlib.Telnet(Host, port=port, timeout=timeout)
+    except:
+        pass
+    finally:
+        tn.close() 
+        return
+
+if __name__ == '__main__':
+    pass
