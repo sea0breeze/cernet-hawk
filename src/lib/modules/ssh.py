765f6e7cfd12b3f19001a0f1a9f6080e8614ca8a
sea0breeze <seabreezefy@gmail.com>
2016-10-21 15:04:46 +0800

    Update ssh.py

--- src/lib/modules/ssh.py
+++ src/lib/modules/ssh.py
@@ -1,23 +1,79 @@
 import socket
+import struct
+from lib.log import cprint
+from cStringIO import StringIO
 
 # refer:
 # https://stribika.github.io/2015/01/04/secure-secure-shell.html
 # http://blog.csdn.net/macrossdzh/article/details/5691924
 # RFC-4253
 
-def ssh_detect(ip, port):
+class sshDetect:
+    '''
+    :str. banner
+    :
+
+    '''
     # not complete yet
-    try:
-        socket.setdefaulttimeout(2)
-        s = socket.socket()
-        s.connect((ip, port))
-        banner = s.recv(50)
-        s.send('SSH-2.0-OpenSSH_6.0p1\r\n')
-        ciphers = s.recv(984)
-        s.close()
-        return banner, ciphers
-    except:
-        return
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        try:
+            socket.setdefaulttimeout(2)
+            s = socket.socket()
+            s.connect((ip, port))
+            self.banner = s.recv(50).strip('\r\n').split(' ')
+
+            try:
+                self.version = self.banner[0]
+                self.os = self.banner[1]
+            except IndexError:
+                pass
+
+            s.send('{}\r\n'.format(self.banner[0]))
+            self._raw_recv = s.recv(984)
+            s.close()
+        except Exception as e:
+            cprint(str(e), 'error')
+            return
+
+    def _parse_raw_data(self):
+        stream = StringIO(self._raw_recv)
+        packet_length = struct.unpack('>i', stream.read(4))[0]
+        padding_length = ord(stream.read(1))
+        message_code = ord(stream.read(1))
+        cookie = stream.read(16)
+        kex_algo_length = struct.unpack('>i', stream.read(4))[0]
+        self.kex_algo_string = stream.read(kex_algo_length).split(',')
+        server_host_key_algo_length = struct.unpack('>i', stream.read(4))[0]
+        self.server_host_key_algo_string = stream.read(server_host_key_algo_length).split(',')
+
+        enc_algo_client_to_server_length = struct.unpack('>i', stream.read(4))[0]
+        self.enc_algo_client_to_server_string = stream.read(enc_algo_client_to_server_length).split(',')
+
+        enc_algo_server_to_client_length = struct.unpack('>i', stream.read(4))[0]
+        self.enc_algo_server_to_client_string = stream.read(enc_algo_server_to_client_length).split(',')
+
+        mac_algo_client_to_server_length = struct.unpack('>i', stream.read(4))[0]
+        self.mac_algo_client_to_server_string = stream.read(mac_algo_client_to_server_length).split(',')
+
+        mac_algo_server_to_client_length = struct.unpack('>i', stream.read(4))[0]
+        self.mac_algo_server_to_client_string = stream.read(mac_algo_server_to_client_length).split(',')
+
+        compress_algo_client_to_server_length = struct.unpack('>i', stream.read(4))[0]
+        self.compress_algo_client_to_server_string = stream.read(compress_algo_client_to_server_length).split(',')
+
+        compress_algo_server_to_client_length = struct.unpack('>i', stream.read(4))[0]
+        self.compress_algo_server_to_client_string = stream.read(compress_algo_server_to_client_length).split(',')
+
+        lang_client_to_server_length = struct.unpack('>i', stream.read(4))[0]
+        self.lang_client_to_server_string = stream.read(lang_client_to_server_length).split(',')
+
+        lang_server_to_client_length = struct.unpack('>i', stream.read(4))[0]
+        self.lang_server_to_client_string = stream.read(lang_server_to_client_length).split(',')
+
 
 if __name__ == '__main__':
-    print sshDetect("ip", 22)
+    ssh_test = sshDetect("0.0.0.0", 22)
+    print ssh_test.banner
+    ssh_test.parse_raw_data()
