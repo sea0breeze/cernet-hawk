d42ebbe84feed98858d9f179e90a0b4aaebe9600
LyleMi <1278919492@qq.com>
2016-10-22 18:48:35 +0800

    update telnet

--- src/lib/modules/telnet.py
+++ src/lib/modules/telnet.py
@@ -1,15 +1,32 @@
+'''
+refer:
+    http://www.cnblogs.com/dazhaxie/archive/2012/06/27/2566054.html
+    ref 854 855
+'''
+
 import telnetlib
+# from lib.log import cprint
 
 
-def telnetDetect(host, port=2611,  timeout=40):
+class telnetDetect:
+
+    '''
+    :str. banner
+    :
+
+    '''
     # not complete yet
-    try:
-        tn = telnetlib.Telnet(Host, port=port, timeout=timeout)
-    except:
-        pass
-    finally:
-        tn.close()
-        return
+
+    def __init__(self, ip, port=53):
+        # not complete yet
+        try:
+            tn = telnetlib.Telnet(host=ip, port=port)
+        except Exception as e:
+            # cprint(str(e), 'error')
+            # print(str(e), 'error')
+            tn.close()
+        finally:
+            return
 
 if __name__ == '__main__':
-    pass
+    telnetTest = telnetDetect("0.0.0.0")
