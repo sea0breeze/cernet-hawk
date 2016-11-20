55bbe156b3a0136eac63a214075a44e7fef17468
LyleMi <1278919492@qq.com>
2016-11-20 15:29:02 +0800

    color test

--- src/lib/scan/color.py
+++ src/lib/scan/color.py
@@ -35,8 +35,15 @@ class colorizing_stream_handler(logging.StreamHandler):
         logging.CRITICAL: ('red', 'white', None)
     }
 
-    csi = ['\033[','\x1b['][platform.system() == "Windows"]
-    reset = ['\033[0m', '\x1b[0m'][platform.system() == "Windows"]
+    if platform.system() == "Windows":
+        csi = '\x1b['
+        reset = '\x1b[0m'
+    elif platform.system() == "Linux":
+        csi = '\033['
+        reset = '\033[0m'
+    else: 
+        csi = '\033['
+        reset = '\033[0m'
 
     def colorize(self, message, record):
         if record.levelno in self.level_map:
