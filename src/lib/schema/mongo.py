0a36aa85006deefd0ec9d43d0a3faf8d2184b5dc
Lyle <tlxsjtu@126.com>
2016-11-20 00:13:33 -0800

    ch mongo

--- src/lib/schema/mongo.py
+++ src/lib/schema/mongo.py
@@ -4,19 +4,24 @@ class Mongo(object):
     """
     MongoDB Client
     """
-    def __init__(self, table):
+    def __init__(self, collection):
         super(Mongo, self).__init__()
         self.conn = MongoClient('localhost')
-        self.col = self.conn.hawk[table]
+        self.col = self.conn.hawk[collection]
 
     def insert(self, data):
         self.col.insert(data)
 
-    def find(self, index):
-        self.col.find(index)
+    def find(self, index = {}):
+        return [i for i in self.col.find(index)]
+
+    def delete(self, index = {}):
+        self.col.remove(index)
 
 if __name__ == '__main__':
     m = Mongo('ftp')
     m.insert({'datajson': 'test'})
     print m.find({})
+    m.delete()
+    print m.find()
 
