fc92c50286b87d3bc75255e0d50c51beb80f2451
LyleMi <1278919492@qq.com>
2016-11-25 15:32:33 +0800

    update http

--- src/lib/modules/http.py
+++ src/lib/modules/http.py
@@ -1,6 +1,10 @@
+from socket import gethostbyaddr
+
 from requests import get
+from whois import whois
 
 from lib.utils.utils import parseUrl
+from lib.scan.log import cprint
 from Detect import Detect
 
 class httpDetect(Detect):
@@ -8,7 +12,26 @@ class httpDetect(Detect):
     def __init__(self, ip, port = 80):
         # init at here
         super(httpDetect, self).__init__(ip, port, 'http')
-        self.data.headers = get(parseUrl(ip)).headers
+        
+        try:
+            r = get(parseUrl(ip))
+            self.data.headers = r.headers
+            self.data.status_code = r.status_code
+            self.data.screenshoot = r.content
+        except:
+            cprint("Connection Error: %s" % ip, "error")
+            return
+        
+        try:
+            self.data.url = gethostbyaddr(ip)[0]
+        except:
+            cprint('Host not found: %s' % ip)
+
+        if hasattr(self.data, 'url'):
+            try:
+                self.data.whois = whois(self.data.url).text
+            except:
+                cprint('Whois not found: %s' % self.data.url)            
 
 if __name__ == '__main__':
     h = httpDetect("localhost")
