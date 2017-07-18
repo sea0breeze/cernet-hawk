3a72a11d90def2e7de97f5f9ec67a193105c6ee1
LyleMi <1278919492@qq.com>
2017-07-18 22:33:51 +0800

    add-db-save

--- orm/ninfo.py
+++ orm/ninfo.py
@@ -9,3 +9,12 @@ class Ninfo(Document):
     ip = StringField(max_length=30, required=True)
     port = IntField(default=0, required=True)
     banner = StringField(max_length=30, required=True)
+
+    @classmethod
+    def save(cls, ip, port, banner):
+        ni = cls()
+        ni.ip = ip
+        ni.port = port
+        ni.banner = banner
+        ni.save()
+        return True
