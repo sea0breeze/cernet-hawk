93e961347c6c0d61a08ec786d6bfe169690c6142
肖子彤 <seabreeze@xiaozitongdeMacBook-Pro.local>
2017-07-19 23:43:27 +0800

    nmap results to mongodb

--- port/runnmap.py
+++ port/runnmap.py
@@ -5,53 +5,14 @@ import subprocess
 import os
 import xml.etree.cElementTree as ET
 import os.path
+from json import dumps
 
 from utils.log import cprint
 import config.paths
 import config.common
 from common.classes.Base import Base
-from orm.schema.mongo import Mongo
+from orm.nmapinfo import NmapInfo
 
-
-def parse_nmap_xml(nmap_xml):
-    """
-    Parse the xml file and extract the imforation of web services.
-    Service info contains (name, version, extrainfo, product, devicetype, ostype...)
-    :param nmap_xml: str. The name of the xml file to parse.
-    :return: dict. Use open port number as key and concrete infomation of the web service as value.
-    """
-
-    errors = []
-    try:
-        tree = ET.parse(nmap_xml)
-        root = tree.getroot()
-        result = {}
-        filter_flag = True
-        for port in root.find('host').find('ports').findall('port'):
-            if port.find('state').get('state') not in  ('filtered', 'closed'):
-                filter_flag = False
-            if port.find('state').get('state') == 'open':
-                service = port.find('service').attrib
-                service.pop('conf')
-                service.pop('method')
-                result[int(port.get('portid'))] = service
-    except Exception as e:
-        cprint(e, 'error')
-        errors.append(nmap_xml)
-        return None
-
-    # What if we get nothing from the xml...
-    if not result:
-        if filter_flag:
-            cprint('All open ports detected by zmap are actually filtered or closed!', 'info')
-            return None
-        else:
-            cprint('Failed to parse nmap xml!', 'error')
-            errors.append(nmap_xml)
-    else:
-        cprint('Nmap xml parsed!', 'debug')
-
-    return result, errors
 
 
 class NmapScan(Base):
@@ -70,6 +31,46 @@ class NmapScan(Base):
             cprint('Nmap is not installed!', 'error')
             exit()
         self.error_list = []
+        self.current_ip = None
+
+    def parse_nmap_xml(self, nmap_xml):
+        """
+        Parse the xml file and extract the imforation of web services.
+        Service info contains (name, version, extrainfo, product, devicetype, ostype...)
+        :param nmap_xml: str. The name of the xml file to parse.
+        :return: dict. Use open port number as key and concrete infomation of the web service as value.
+        """
+
+        try:
+            tree = ET.parse(nmap_xml)
+            root = tree.getroot()
+            result = {}
+            filter_flag = True
+            for port in root.find('host').find('ports').findall('port'):
+                if port.find('state').get('state') not in  ('filtered', 'closed'):
+                    filter_flag = False
+                if port.find('state').get('state') == 'open':
+                    service = port.find('service').attrib
+                    service.pop('conf')
+                    service.pop('method')
+                    result[port.get('portid')] = service
+        except Exception as e:
+            cprint(e, 'error')
+            self.error_list.append(self.current_ip)
+            return None
+
+        # What if we get nothing from the xml...
+        if not result:
+            if filter_flag:
+                cprint('All open ports detected by zmap are actually filtered or closed!', 'warning')
+            else:
+                cprint('Failed to parse nmap xml!', 'error')
+                self.error_list.append(self.current_ip)
+            return None
+        else:
+            cprint('Nmap xml parsed!', 'debug')
+
+        return result
 
     def single_run_nmap(self, ip, ports):
         """
@@ -80,16 +81,17 @@ class NmapScan(Base):
         :param port: list. All open ports of the ip founded by zmap.
         :return: list. Further imformation about services on the host.
         """
-
+        
+        self.current_ip = ip
         filename = os.path.join(config.paths.logpath, '{}_{}.xml.log'.format(ip, '-'.join(map(str, ports))))
         # Save time while debugging by not scanning ip that has been scanned.
         if config.common.DEBUG and os.path.exists(filename):
             cprint('Previous scan result exists for {}, just parse the xml.'.format(ip), 'info')
-            return parse_nmap_xml(filename)
+            return self.parse_nmap_xml(filename)
 
         os.seteuid(0)  # run as root.
         cprint('Start scanning {} with nmap'.format(ip), 'info')
-        cmd = [self.nmap_path] + config.common.NMAP_CMD + [ip, '-p', ','.join(map(str, ports))]
+        cmd = [self.nmap_path] + config.common.NMAP_CMD + [ip, '-p', ','.join(ports)]
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, error = p.communicate()
 
@@ -103,8 +105,7 @@ class NmapScan(Base):
         f = open(filename, 'w+')
         f.write(out)
         f.close()
-        result, self.error_list = parse_nmap_xml(filename)
-        return result
+        return self.parse_nmap_xml(filename)
 
     def run(self, ipdir):
         """
@@ -128,7 +129,7 @@ class NmapScan(Base):
             cprint('One ip range scanning finished with no error!', 'info')
 
         self.nmap_result = result
-        print result
+        NmapInfo.addWithJson(dumps(result))
 
 
 if __name__ == "__main__":
