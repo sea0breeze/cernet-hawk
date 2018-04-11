075abab5b69e1d72fe385035a5415da9b4eae043
sea0breeze <seabreezefy@gmail.com>
2018-04-11 14:56:13 +0800

    Add tls for pop3

--- services/pop3.py
+++ services/pop3.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import ssl
 import socket
 
 from utils.log import cprint
+from utils.cert import parse_der
 from common.classes.PortBase import PortBase
 from orm.servicesinfo import ServicesInfo
 
@@ -32,7 +34,22 @@ class pop3Detect(PortBase):
             self.clear()
             return None
 
+        if 'STLS' in self.data.capability_list:
+            try:
+                s.send("STLS\r\n")
+                resp = s.recv(1024)
+                if resp[:3] == '+OK':
+                    ss = ssl.wrap_socket(s)
+                    cert = ss.getpeercert(True)
+                    self.data.update(parse_der(cert))
+            except Exception as e:
+                cprint(str(e), 'error')
+                self.clear()
+                return None
+
         s.close()
+        # from pprint import pprint
+        # pprint(self.data)
         ServicesInfo.add(ip, port, 'pop3', self.data)
         self.clear()
         return True
