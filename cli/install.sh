45d61b8a99e1e33870724a4183ff73f68544919d
root <root@vultr.guest>
2018-04-12 05:38:05 +0000

    convert \r\n to \n

--- cli/install.sh
+++ cli/install.sh
@@ -1,9 +1,10 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install zmap
-sudo apt install nmap
-sudo apt install python-pip
-sudo apt install mongo
-sudo apt install rabbitmq-server
+sudo apt install zmap -y 
+sudo apt install nmap -y 
+sudo apt install python-pip -y
+sudo apt install mongo -y 
+sudo apt install rabbitmq-server -y 
 sudo pip install --upgrade pip
-sudo pip install -r requirements.txt
+sudo pip install setuptools
+sudo pip install -r requirements.txt 
